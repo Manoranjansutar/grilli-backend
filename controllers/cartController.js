1941a5dd3349db7b1480604276d2623320805b18
MANORANJAN SUTAR <149157746+Manoranjansutar@users.noreply.github.com>
2024-06-27 12:15:50 +0530

    first commit

---
+++ controllers/cartController.js
@@ -0,0 +1,53 @@
+import { response } from 'express';
+import userModel from '../models/userModel.js'
+
+//add item to cart
+const addToCart = async(req,res) =>{
+    try {
+       let userData = await userModel.findById(req.body.userId);
+       let cartData = await userData.cartData;
+       if(!cartData[req.body.itemId]){
+          cartData[req.body.itemId] = 1;
+       } else {
+          cartData[req.body.itemId] += 1;
+       }
+       await userModel.findByIdAndUpdate(req.body.userId,{cartData})
+       res.json({success:true,message:"addded to cart"})
+   } catch (error) {
+       console.log(error)
+       res.json({success:false,message:"Error"})
+   }
+}
+
+
+//remove item from cart
+const removeFromCart = async(req,res) =>{
+   try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if(cartData[req.body.itemId]>0){
+        cartData[req.body.itemId] -= 1
+    }
+    await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+    res.json({success:true,message:"Removed from cart"})
+   } catch (error) {
+      console.log(error)
+      res.json({success:false,message:"Error"})
+   }
+}
+
+
+//fetch data from Cart
+const getCart = async(req,res) =>{
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        res.json({success:true,cartData})
+    } catch (error) {
+        console.log(data) 
+        res.json({success:false,message:"Error"})
+    }
+}
+
+
+export {addToCart,removeFromCart,getCart}
